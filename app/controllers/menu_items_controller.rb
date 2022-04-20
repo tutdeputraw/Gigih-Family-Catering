d78cc9e7fe92ccd26703170927f0375b0fe5487e
tutdeputraw <anoboy.code@gmail.com>
2022-04-20 16:15:20 +0700

    create an API to store a menu item together with its associations

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,3 +1,33 @@
 class MenuItemsController < ApplicationController
+  def index
+    @menu_items = MenuItem.all
 
+    render :json => {
+      data: @menu_items
+    }, include: [
+      :menu_categories => { :only => [:id, :name] }
+    ]
+  end
+
+  def create
+    @menu_item = MenuItem.new(menu_item_params)
+    @menu_item.menu_categories << MenuCategory.where(id: params['menu_category_id'])
+
+    if @menu_item.save
+      render :json => {
+        message: 'new record created successfully',
+        data: @menu_item
+      }
+    else
+      render :json => {
+        message: 'create new record failed',
+      }
+    end
+  end
+
+  private
+
+  def menu_item_params
+    params.permit(:name, :price, :description)
+  end
 end
