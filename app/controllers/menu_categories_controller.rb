6cc712c1743d85df3a43c47d6df6fa3eecfcd242
tutdeputraw <anoboy.code@gmail.com>
2022-04-20 12:44:30 +0700

    add an API to create a menu category record

--- app/controllers/menu_categories_controller.rb
+++ app/controllers/menu_categories_controller.rb
@@ -1,2 +1,18 @@
 class MenuCategoriesController < ApplicationController
+  def create
+    @menu_category = MenuCategory.new(menu_category_params)
+
+    if @menu_category.save
+      render :json => {
+        message: 'menu created',
+        data: @menu_category
+      }
+    end
+  end
+
+  private
+
+  def menu_category_params
+    params.require(:menu_category).permit(:name)
+  end
 end
