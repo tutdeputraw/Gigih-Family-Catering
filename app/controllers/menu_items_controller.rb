ea0c9de96e38749058ac83d1e5846c7091414e09
tutdeputraw <anoboy.code@gmail.com>
2022-04-20 19:14:47 +0700

    add the show method in the menu items controller

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -9,6 +9,16 @@ class MenuItemsController < ApplicationController
     ]
   end
 
+  def show
+    @menu_items = MenuItem.find(params['id'])
+
+    render :json => {
+      data: @menu_items
+    }, include: [
+      :menu_categories => { :only => [:id, :name] }
+    ]
+  end
+
   def create
     @menu_item = MenuItem.new(menu_item_params)
     @menu_item.menu_categories << MenuCategory.where(id: params['menu_category_id'])
