253c0ad74cb2826f7b57fbf0b042b4d89222e00f
tutdeputraw <anoboy.code@gmail.com>
2022-04-20 12:52:30 +0700

    create an API to get the record of menu categories

--- app/controllers/menu_categories_controller.rb
+++ app/controllers/menu_categories_controller.rb
@@ -1,4 +1,12 @@
 class MenuCategoriesController < ApplicationController
+  def index
+    @menu_categories = MenuCategory.all
+
+    render :json => {
+      data: @menu_categories
+    }
+  end
+
   def create
     @menu_category = MenuCategory.new(menu_category_params)
 
