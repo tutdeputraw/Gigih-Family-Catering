c7b65f737429959e9d38c16e2780a0ef2efcd909
tutdeputraw <anoboy.code@gmail.com>
2022-04-20 20:09:05 +0700

    add resources for the orders model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :menu_items
   resources :menu_categories
+  resources :orders
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
