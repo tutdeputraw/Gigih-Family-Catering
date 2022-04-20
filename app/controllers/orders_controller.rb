9ca6004f36b7ae2dc05e9300ca9dfd51a0c6c5c0
tutdeputraw <anoboy.code@gmail.com>
2022-04-21 00:00:00 +0700

    add an API to store user orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,29 @@
 class OrdersController < ApplicationController
+  def create
+    @orders = Order.create(
+      email: params['email'],
+      total_price: 0,
+      status: 'NEW'
+    )
+
+    params[:items].each do |item|
+      menu_item = MenuItem.find(item[:id])
+
+      OrderItem.create(
+        menu_item: menu_item,
+        order: @orders,
+        item_price: menu_item.price,
+        quantity: item[:quantity]
+      )
+    end
+
+    render :json => {
+      message: 'new record successfully created',
+      data: @orders
+    }, include: [
+      :order_items
+    ]
+  end
+
+  private
 end
