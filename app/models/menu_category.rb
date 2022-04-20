ddbad948afc15744918cb161531a097c30a9d431
tutdeputraw <anoboy.code@gmail.com>
2022-04-20 11:31:19 +0700

    add associations in models

--- app/models/menu_category.rb
+++ app/models/menu_category.rb
@@ -1,2 +1,4 @@
 class MenuCategory < ApplicationRecord
+  has_many :item_categories
+  has_many :menu_items, through: :item_categories
 end
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,2 +1,4 @@
 class MenuItem < ApplicationRecord
+  has_many :item_categories
+  has_many :menu_categories, through: :item_categories
 end
