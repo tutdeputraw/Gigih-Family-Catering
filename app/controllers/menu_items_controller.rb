717e97e26d090db31563f9552e309896a0567077
tutdeputraw <anoboy.code@gmail.com>
2022-04-20 19:45:46 +0700

    add destroy and update methods in the menu item controller

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,36 +1,61 @@
 class MenuItemsController < ApplicationController
   def index
-    @menu_items = MenuItem.all
+    menu_items = MenuItem.all
 
     render :json => {
-      data: @menu_items
+      message: '',
+      data: menu_items
     }, include: [
       :menu_categories => { :only => [:id, :name] }
     ]
   end
 
   def show
-    @menu_items = MenuItem.find(params['id'])
+    menu_item = MenuItem.find(params['id'])
 
     render :json => {
-      data: @menu_items
+      message: '',
+      data: menu_item
     }, include: [
       :menu_categories => { :only => [:id, :name] }
     ]
   end
 
   def create
-    @menu_item = MenuItem.new(menu_item_params)
-    @menu_item.menu_categories << MenuCategory.where(id: params['menu_category_id'])
+    menu_item = MenuItem.new(menu_item_params)
+    menu_item.menu_categories << MenuCategory.where(id: params['menu_category_id'])
 
-    if @menu_item.save
+    if menu_item.save
       render :json => {
-        message: 'new record created successfully',
-        data: @menu_item
+        message: 'new record successfully created',
+        data: menu_item
       }
-    else
+    end
+  end
+
+  def update
+    menu_item = MenuItem.find(params['id'])
+
+    menu_item.update(menu_item_params)
+    menu_item.menu_categories.replace(MenuCategory.where(id: params['menu_category_id']))
+
+    if menu_item.save
       render :json => {
-        message: 'create new record failed',
+        message: 'the record successfully updated',
+        data: menu_item
+      }, include: [
+        :menu_categories => { :only => [:id, :name] }
+      ]
+    end
+  end
+
+  def destroy
+    menu_item = MenuItem.find(params['id'])
+    menu_item.menu_categories.clear
+
+    if menu_item.destroy
+      render :json => {
+        message: 'the record successfully deleted',
       }
     end
   end
