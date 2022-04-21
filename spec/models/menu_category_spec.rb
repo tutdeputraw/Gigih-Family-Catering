65f024ce4aa2f48c5af6ec86c2518297c1356bf7
tutdeputraw <anoboy.code@gmail.com>
2022-04-21 20:21:43 +0700

    add tests of the MenuCategory model to test associations

--- spec/models/menu_category_spec.rb
+++ spec/models/menu_category_spec.rb
@@ -1,1 +1,8 @@
+require 'rails_helper'
 
+RSpec.describe MenuCategory, type: :model do
+  describe 'associations' do
+    it { should have_many(:menu_items).class_name('MenuItem') }
+    it { should have_many(:item_categories).class_name('ItemCategory') }
+  end
+end
