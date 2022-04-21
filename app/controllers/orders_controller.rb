f4c31c5332f7567c6740b0a832a2aca733448916
tutdeputraw <anoboy.code@gmail.com>
2022-04-21 16:09:28 +0700

    add index method in the order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,27 @@
 class OrdersController < ApplicationController
+  def index
+    if has_email_param?
+      @orders = Order.where(:email => params[:email])
+    elsif has_total_price_param?
+      @orders = Order.where('total_price > ?', params[:total_price])
+    elsif has_range_date_param?
+      @orders = Order.where(:created_at => params[:start_date]..params[:end_date])
+    else
+      @orders = Order.all
+    end
+
+    render :json => {
+      message: '',
+      data: @orders
+    }, :include => [
+      :order_items
+    ]
+  end
+
   def create
     @orders = Order.create(
       email: params['email'],
-      total_price: 0,
+      total_price: 233,
       status: 'NEW'
     )
 
@@ -26,4 +45,17 @@ class OrdersController < ApplicationController
   end
 
   private
+
+  def has_email_param?
+    params.has_key?(:email)
+  end
+
+  def has_total_price_param?
+    params.has_key?(:total_price)
+  end
+
+  def has_range_date_param?
+    params.has_key?(:start_date) && params.has_key?(:end_date)
+  end
 end
+
