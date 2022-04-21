4dd9958c6f6b466ae7ad5570a692b702ce814f00
tutdeputraw <anoboy.code@gmail.com>
2022-04-21 19:48:20 +0700

    change the variable name of menu_categories to @menu_categories

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,48 +1,48 @@
 class MenuItemsController < ApplicationController
   def index
-    menu_items = MenuItem.all
+    @menu_items = MenuItem.all
 
     render :json => {
       message: '',
-      data: menu_items
+      data: @menu_items
     }, include: [
       :menu_categories => { :only => [:id, :name] }
     ]
   end
 
   def show
-    menu_item = MenuItem.find(params['id'])
+    @menu_items = MenuItem.find(params['id'])
 
     render :json => {
       message: '',
-      data: menu_item
+      data: @menu_items
     }, include: [
       :menu_categories => { :only => [:id, :name] }
     ]
   end
 
   def create
-    menu_item = MenuItem.new(menu_item_params)
-    menu_item.menu_categories << MenuCategory.where(id: params['menu_category_id'])
+    @menu_items = MenuItem.new(menu_item_params)
+    @menu_items.menu_categories << MenuCategory.where(id: params['menu_category_id'])
 
-    if menu_item.save
+    if @menu_items.save
       render :json => {
         message: 'new record successfully created',
-        data: menu_item
+        data: @menu_items
       }
     end
   end
 
   def update
-    menu_item = MenuItem.find(params['id'])
+    @menu_items = MenuItem.find(params['id'])
 
-    menu_item.update(menu_item_params)
-    menu_item.menu_categories.replace(MenuCategory.where(id: params['menu_category_id']))
+    @menu_items.update(menu_item_params)
+    @menu_items.menu_categories.replace(MenuCategory.where(id: params['menu_category_id']))
 
-    if menu_item.save
+    if @menu_items.save
       render :json => {
         message: 'the record successfully updated',
-        data: menu_item
+        data: @menu_items
       }, include: [
         :menu_categories => { :only => [:id, :name] }
       ]
@@ -50,10 +50,10 @@ class MenuItemsController < ApplicationController
   end
 
   def destroy
-    menu_item = MenuItem.find(params['id'])
-    menu_item.menu_categories.clear
+    @menu_items = MenuItem.find(params['id'])
+    @menu_items.menu_categories.clear
 
-    if menu_item.destroy
+    if @menu_items.destroy
       render :json => {
         message: 'the record successfully deleted',
       }
