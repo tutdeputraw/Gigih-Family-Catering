b7b4c35c792dfaaf1001cb6285d556faa90d772b
tutdeputraw <anoboy.code@gmail.com>
2022-04-21 22:52:31 +0700

    set value cascade to the onDelete property of the menu item model

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,5 +1,5 @@
 class MenuItem < ApplicationRecord
-  has_many :item_categories
+  has_many :item_categories, dependent: :destroy
   has_many :menu_categories, through: :item_categories
 
   has_many :order_items
