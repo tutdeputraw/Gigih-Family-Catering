b7f5d5918a1229716c65ecdcc91f8a0e40c74641
tutdeputraw <anoboy.code@gmail.com>
2022-04-22 12:38:06 +0700

    add validation of the email property

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
   has_many :order_items
   has_many :menu_items, :through => :order_items
+
+  validates :email, presence: true, format: {
+    with: /\A[\w+\-.]+@[a-z\d\-.]+\.[c][o][m]\z/,
+    message: 'wrong format'
+  }
 end
