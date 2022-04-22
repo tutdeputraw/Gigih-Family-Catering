3645c2f3bdb7d840702a6fb6b47fa97a730453bf
tutdeputraw <anoboy.code@gmail.com>
2022-04-22 11:10:12 +0700

    add a try-catch to each method in the order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,35 +1,43 @@
 class OrdersController < ApplicationController
   def index
-    if has_email_param?
-      @orders = Order.where(:email => params[:email])
-    elsif has_total_price_param?
-      @orders = Order.where('total_price > ?', params[:total_price])
-    elsif has_range_date_param?
-      @orders = Order.where(:created_at => params[:start_date]..params[:end_date])
-    else
-      @orders = Order.all
-    end
+    begin
+      if has_email_param?
+        @orders = Order.where(:email => params[:email])
+      elsif has_total_price_param?
+        @orders = Order.where('total_price > ?', params[:total_price])
+      elsif has_range_date_param?
+        @orders = Order.where(:created_at => params[:start_date]..params[:end_date])
+      else
+        @orders = Order.all
+      end
 
-    render :json => {
-      message: '',
-      data: @orders
-    }, :include => [
-      :order_items
-    ]
+      render :json => {
+        message: '',
+        data: @orders
+      }, :include => [
+        :order_items
+      ]
+    rescue Exception => e
+      render_error(e.message)
+    end
   end
 
   def create
-    create_order
-    create_order_items
+    begin
+      create_order
+      create_order_items
 
-    update_total_price(get_total_price)
+      update_total_price(get_total_price)
 
-    render :json => {
-      message: 'new record successfully created',
-      data: @orders
-    }, include: [
-      :order_items => { :only => [:menu_item_id, :item_price, :quantity] }
-    ]
+      render :json => {
+        message: 'new record successfully created',
+        data: @orders
+      }, include: [
+        :order_items => { :only => [:menu_item_id, :item_price, :quantity] }
+      ]
+    rescue Exception => e
+      render_error(e.message)
+    end
   end
 
   private
@@ -44,6 +52,13 @@ class OrdersController < ApplicationController
 
   def has_range_date_param?
     params.has_key?(:start_date) && params.has_key?(:end_date)
+  end
+
+  def render_error(error)
+    render :json => {
+      message: 'error has occurred',
+      errors: error
+    }
   end
 
   def create_order
